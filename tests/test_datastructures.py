5065ed91e0b53306eea04c8a6a8eaa99640d8f81
Randy Syring <rsyring@gmail.com>
2010-06-14 17:48:11 -0400

    added LazyDict

--- tests/test_datastructures.py
+++ tests/test_datastructures.py
@@ -1,5 +1,5 @@
 from pysutils.datastructures import DumbObject, OrderedProperties, OrderedDict, \
-    HtmlAttributeHolder, LazyOrderedDict
+    HtmlAttributeHolder, LazyOrderedDict, LazyDict
 
 def test_dumbobject():
     assert DumbObject(a=1).a == 1
@@ -30,6 +30,21 @@ def test_ordereddict_lazy():
             assert v == 1
         else:
             assert v == 2, v
+    assert o.a == 1
+    assert o.b == 2
+    o.c = 3
+    assert o.c == 3, o.c
+    assert o['c'] == 3
+    try:
+        o.d
+        assert False
+    except AttributeError:
+        pass
+
+
+def test_lazy_dict():
+
+    o = LazyDict(a=1, b=2)
     assert o.a == 1
     assert o.b == 2
     o.c = 3
