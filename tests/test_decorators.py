0b2ff505f46e470d38f192b048c7d6edba4d1b49
Randy Syring <rsyring@gmail.com>
2010-06-15 11:49:28 -0400

    whitespace change

---
+++ tests/test_decorators.py
@@ -0,0 +1,23 @@
+from pysutils import curry, decorator
+
+def test_curry():
+    
+    @curry
+    def myfunc(a, b, c):
+        return a+b+c;
+    
+    f = myfunc(1)
+    f = f(2)
+    assert f(3) == 6
+    
+def test_decorator():
+    
+    @decorator
+    def mydec(fn, toadd):
+        return 4 + fn(toadd)
+    
+    @mydec
+    def myfunc(toreturn):
+        return toreturn
+    
+    assert myfunc(5) == 9
