3581514597860c4311f71d558c6d2955b3ef41b5
Randy Syring <rsyring@gmail.com>
2010-06-22 10:43:15 -0400

    setup.py changes

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ Introduction
 
 BlazeUtils is a library to hold common tools for the Blaze library family:
 
-- `pysmvt <http://pypi.python.org/pypi/BlazeWeb/>`_
-- `pysapp <http://pypi.python.org/pypi/TioBWA/>`_
-- `pysform <http://pypi.python.org/pypi/BlazeForm/>`_
+- `BlazeWeb <http://pypi.python.org/pypi/BlazeWeb/>`_
+- `BaseBWA <http://pypi.python.org/pypi/BaseBWA/>`_
+- `BlazeForm <http://pypi.python.org/pypi/BlazeForm/>`_
 
 Questions & Comments
 ---------------------
 
-Please visit: http://groups.google.com/group/pyslibs
+Please visit: http://groups.google.com/group/blazelibs
 
 Current Status
 ---------------
 
-The code stays pretty stable, but the API is likely to change in the future.
+The code and API stay pretty stable.
 
 The `blazeutils tip <http://bitbucket.org/rsyring/blazeutils/get/tip.zip#egg=blazeutils-dev>`_
 is installable via `easy_install` with ``easy_install blazeutils==dev``
