e261744332146ef73505dcc5e270668c4dd3c814
Randy Syring <rsyring@gmail.com>
2010-07-13 17:59:46 -0400

    QuickSettings get __copy__() and setdefaults()

--- blazeutils/config.py
+++ blazeutils/config.py
@@ -1,22 +1,23 @@
+import copy
 from blazeutils.datastructures import OrderedProperties, OrderedDict
 
 class QuickSettings(OrderedProperties):
     def __init__(self, initialize=True):
         self._locked = False
         OrderedProperties.__init__(self, initialize)
-    
+
     def lock(self):
         self._locked = True
         for child in self._data.values():
             if isinstance(child, QuickSettings):
                 child.lock()
-    
+
     def unlock(self):
         self._locked = False
         for child in self._data.values():
             if isinstance(child, QuickSettings):
                 child.unlock()
-    
+
     def __getattr__(self, key):
         if not self._data.has_key(key):
             if not self._locked:
@@ -24,7 +25,7 @@ class QuickSettings(OrderedProperties):
             else:
                 raise AttributeError("object has no attribute '%s' (object is locked)" % key)
         return self._data[key]
-    
+
     def update(self, ____sequence=None, **kwargs):
         if ____sequence is not None:
             if hasattr(____sequence, 'keys'):
@@ -40,13 +41,13 @@ class QuickSettings(OrderedProperties):
                     self[key] = value
         if kwargs:
             self.update(kwargs)
-    
+
     def set_dotted(self, key, value):
         """
                 qs.set_dotted('foo.bar', 'baz')
-            
+
             is equivelent to:
-            
+
                 qs.foo.bar = baz
         """
         parts = key.split('.')
@@ -56,13 +57,13 @@ class QuickSettings(OrderedProperties):
             for name in parts:
                 cobj = getattr(cobj, name)
         setattr(cobj, key, value)
-        
+
     def get_dotted(self, key):
         """
                 obj = qs.get_dotted('foo.bar.baz')
-            
+
             is equivelent to:
-            
+
                 obj = qs.foo.bar.baz
         """
         parts = key.split('.')
@@ -70,7 +71,7 @@ class QuickSettings(OrderedProperties):
         for attr in parts:
             cobj = getattr(cobj, attr)
         return cobj
-    
+
     def expandkeys(self):
         retval = OrderedDict()
         for k in self.keys():
@@ -82,7 +83,38 @@ class QuickSettings(OrderedProperties):
             else:
                 retval[k] = v
         return retval
-    
+
+    def __copy__(self):
+        """
+            facilitates copying of the object so that no QS objects are the
+            same but values are copied by reference
+        """
+        qs = QuickSettings()
+        for k in self.keys():
+            v = self[k]
+            if isinstance(v, QuickSettings):
+                qs[k] = copy.copy(v)
+            else:
+                qs[k] = v
+        return qs
+
+    def setdefaults(self, from_qs):
+        """
+            sets values from a QuickSettings object, only keeping values that
+            are not already defined on the main object
+        """
+        for k in from_qs.keys():
+            from_value = from_qs[k]
+            fv_is_qs = isinstance(from_value, QuickSettings)
+            self_value = self.get(k, None)
+            sv_is_qs = isinstance(self_value, QuickSettings)
+            if self_value is None:
+                # we don't have a value, so just take the value of the from
+                # object
+                self[k] = from_value
+            elif sv_is_qs and fv_is_qs:
+                self_value.setdefaults(from_value)
+
     @property
     def pformat(self):
         retval = ''
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,15 +1,17 @@
-from nose.tools import eq_
+import copy
+
 from blazeutils.config import QuickSettings
+from nose.tools import eq_
 
 class Base(QuickSettings):
-    
+
     def __init__(self):
         QuickSettings.__init__(self)
-        
+
         # name of the website/application
         self.name.full = 'full'
         self.name.short = 'short'
-        
+
         # application modules from our application or supporting applications
         self.modules.users.enabled = True
         self.modules.users.var2 = 'not bar'
@@ -21,21 +23,21 @@ class Base(QuickSettings):
         self.modules.users.level5.level2.var1 = 'not_bar'
         self.modules.apputil.enabled = True
         self.modules.inactivemod.enabled = False
-        
+
         #######################################################################
         # ROUTING
         #######################################################################
         # default routes
         self.routing.routes = [1, 2]
-        
+
         # route prefix
         self.routing.prefix = ''
-        
+
         #######################################################################
         # DATABASE
         #######################################################################
         self.db.echo = False
-        
+
         #######################################################################
         # SESSIONS
         #######################################################################
@@ -50,40 +52,40 @@ class Base(QuickSettings):
         self.template.default = 'default.html'
         self.template.admin = 'admin.html'
         self.trap_view_exceptions = True
-        
+
         #######################################################################
         # LOGGING & DEBUG
         #######################################################################
         # currently support 'debug' & 'info'
         self.logging.levels = ()
-        
+
         # no more values can be added
         self.lock()
-        
+
 class Default(Base):
 
     def __init__(self):
         Base.__init__(self)
-        
+
         # supporting applications
         self.supporting_apps = ['rcsappbase']
-        
+
         # application modules from our application or supporting applications
         self.unlock()
         self.modules.contentbase.enabled = True
         self.modules.lagcontent.enabled = True
         self.lock()
-        
+
         #######################################################################
         # ROUTING
         #######################################################################
         self.routing.routes.extend([3,4])
-        
+
         #######################################################################
         # DATABASE
         #######################################################################
         self.db.echo = True
-        
+
         #######################################################################
         # LOGGING & DEBUG
         #######################################################################
@@ -92,32 +94,32 @@ class Default(Base):
         self.hide_exceptions = False
 
 class UserSettings(QuickSettings):
-    
+
     def __init__(self):
         QuickSettings.__init__(self)
-        
+
         self.routes = ([
             '/test1',
             '/test2',
         ])
-        
+
         self.var1 = 'foo'
         self.var2 = 'bar'
-        
+
         self.level2.var1 = 'foo'
         self.level2.var2 = 'bar'
-        
+
         self.level3.var1 = 'foo'
         self.level3.var2 = 'bar'
-        
+
         self.level4.var1 = 'foo'
         self.level4.var2 = 'bar'
-        
+
         self.level5.level1.var1 = 'foo'
         self.level5.level2.var1 = 'bar'
         self.level5.level2.var2 = 'baz'
         self.level5.level3.var1 = 'bob'
-        
+
         # no more values can be added
         self.lock()
 
@@ -127,52 +129,52 @@ class TestQuickSettings(object):
         es = QuickSettings()
         es.a = 1
         assert es.a == 1
-        
+
     def test_level2(self):
         es = QuickSettings()
         es.a.a = 1
         assert es.a.a == 1
-        
+
     def test_email(self):
         es = QuickSettings()
         es.email.smtp.server = 'example.com'
         es.email.smtp.user_name = 'myself'
         es.email.smtp.password = 'pass'
-        
+
         assert es.email.smtp.server == 'example.com'
         assert es.email.smtp.user_name == 'myself'
         assert es.email.smtp.password == 'pass'
-        
+
     def test_settings(self):
         s = Default()
-        
+
         assert s.name.full == 'full'
         assert s.name.short == 'short'
         assert s.modules.keys() == ['users', 'apputil','inactivemod','contentbase', 'lagcontent'], s.modules.keys()
         assert s.routing.routes == [1,2,3,4]
-        
+
         assert s.db.echo == True
-    
+
         assert s.logging.levels == ('info', 'debug')
         assert s.trap_view_exceptions == False
         assert s.hide_exceptions == False
-        
+
         assert s.template.default == 'default.html'
         assert s.template.admin == 'admin.html'
-        
+
         assert s.beaker.type == 'dbm'
         assert s.beaker.data_dir == 'session_cache'
 
     def test_lock(self):
         s = Default()
-        
+
         try:
             foo = s.not_there
         except AttributeError, e:
             assert str(e) == "object has no attribute 'not_there' (object is locked)"
         else:
             assert False, "lock did not work, expected AttributeError"
-        
+
         # make sure lock went to children
         try:
             foo = s.db.not_there
@@ -184,22 +186,22 @@ class TestQuickSettings(object):
     def test_unlock(self):
         s = Default()
         s.unlock()
-        
+
         s.new_attr = 'new_attr'
         s.db.new_attr = 'new_attr'
-        
+
         assert s.db.new_attr == 'new_attr'
         assert s.new_attr == 'new_attr'
-        
+
         s.lock()
-        
+
         try:
             foo = s.not_there
         except AttributeError, e:
             assert str(e) == "object has no attribute 'not_there' (object is locked)"
         else:
             assert False, "lock did not work, expected AttributeError"
-        
+
         # make sure lock went to children
         try:
             foo = s.db.not_there
@@ -207,57 +209,57 @@ class TestQuickSettings(object):
             assert str(e) == "object has no attribute 'not_there' (object is locked)"
         else:
             assert False, "lock did not work on child, expected AttributeError"
-        
+
     def test_dict_convert(self):
         s = Default()
-        
+
         # beaker would need a dictionary, so lets see if it works
         d = {
             'type' : 'dbm',
             'data_dir' : 'session_cache'
         }
-        
+
         assert dict(s.beaker) == d
         assert s.beaker.todict() == d
-    
+
     def test_hasattr(self):
         s = Default()
-        
+
         assert hasattr(s, 'alajsdf') == False
         assert hasattr(s, 'alajsdf') == False
-        
+
         s.unlock()
-        assert hasattr(s, 'alajsdf') == True        
-    
+        assert hasattr(s, 'alajsdf') == True
+
     def test_modules(self):
         s = Default()
-        
+
         s.unlock()
         s.modules.fatfingeredmod.enabledd = True
         s.lock()
-        
+
         allmods = mods = ['users', 'apputil', 'inactivemod', 'contentbase', 'lagcontent', 'fatfingeredmod']
         eq_( mods, s.modules.keys() )
-        
+
         eq_( len(mods), len([v for v in s.modules]))
         eq_( len(mods), len(s.modules))
-        
+
         eq_( len(mods), len(s.modules.todict()))
         eq_( len(allmods), len(s.modules.todict()))
-        
+
         assert 'users' in s.modules
-    
+
     def test_merge(self):
         s = Default()
         us = UserSettings()
-        
+
         try:
             eq_(s.modules.users.var1, 'foo')
         except AttributeError, e:
             assert str(e) == "object has no attribute 'var1' (object is locked)"
         else:
             assert False, "expected AttributeError for 'var1'"
-    
+
         eq_(s.modules.users.var2, 'not bar')
         eq_(us.var2, 'bar')
         eq_(len(us.routes), 2)
@@ -269,10 +271,10 @@ class TestQuickSettings(object):
         eq_(us.level5.level2.var1, 'bar')
         eq_(us.level5.level2.var2, 'baz')
         eq_(us.level5.level3.var1, 'bob')
-        
+
         us.update(s.modules.users)
         s.modules['users'] = us
-        
+
         eq_(s.modules.users.var2, 'not bar')
         eq_(s.modules.users.var1, 'foo')
         eq_(len(s.modules.users.routes), 0)
@@ -285,7 +287,7 @@ class TestQuickSettings(object):
         eq_(s.modules.users.level5.level1.var1.notlikely, 'foo')
         eq_(s.modules.users.level5.level2.var1, 'not_bar')
         eq_(s.modules.users.level5.level2.var2, 'baz')
-        
+
         eq_(s.modules.users.enabled, True)
 
 def test_set_dotted():
@@ -293,12 +295,12 @@ def test_set_dotted():
     qs.set_dotted('foo.bar.wow', 'baz')
     qs.lock()
     assert qs.foo.bar.wow == 'baz'
-    
+
     qs = QuickSettings()
     qs.set_dotted('foo', 'baz')
     qs.lock()
     assert qs.foo == 'baz'
-    
+
     qs = QuickSettings()
     qs.set_dotted('', 'baz')
 
@@ -307,17 +309,42 @@ def test_get_dotted():
     qs.set_dotted('foo.bar.wow', 'baz')
     qs.lock()
     eq_(qs.get_dotted('foo.bar.wow'), 'baz')
-    
+
     eq_(qs.get_dotted('foo.bar'), qs.foo.bar)
-    
+
     eq_(qs.get_dotted('foo'), qs.foo)
-    
+
     try:
         qs.get_dotted('foo.none')
     except AttributeError, e:
         if 'none' not in str(e):
             raise
-    
+
     qs.unlock()
     assert isinstance(qs.get_dotted('foo.none'), QuickSettings)
-    
+
+def test_copy():
+    qs = QuickSettings()
+    mylist = []
+    qs.foo.bar.baz = mylist
+
+    qs2 = copy.copy(qs)
+    assert qs2.foo.bar.baz is mylist
+    assert qs.foo.bar.baz is mylist
+    assert qs.foo is not qs2.foo
+    assert qs.foo.bar is not qs2.foo.bar
+
+def test_setdefault():
+    main = QuickSettings()
+    mylist = []
+    main.foo.bar.baz = mylist
+
+    qs = QuickSettings()
+    qs.one = 1
+    qs.foo.bar.baz = 2
+    qs.foo.bar.three = 3
+
+    main.setdefaults(qs)
+    assert main.foo.bar.baz is mylist
+    assert main.foo.bar.three == 3
+    assert main.one == 1
