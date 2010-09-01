ddc591ced9de13411bc8d11546e81c4577b87e49
Randy Syring <rsyring@gmail.com>
2010-09-01 17:24:17 -0400

    added awrite() method to XlwtHelper to manage row/column position automatically.  Use like:
            # main heading
            xlh.awrite(proj.name, font_heading1, nextrow=True)
            xlh.awrite(nextrow=True)
    
            # Project Info
            xlh.awrite('Project Info', font_heading2, nextrow=True)
            xlh.awrite('type')
            xlh.awrite(proj.type, nextrow=True)
            xlh.awrite('action')
            xlh.awrite(proj.action, nextrow=True)
            xlh.awrite('charge type')
            xlh.awrite(proj.charge_type, nextrow=True)
            xlh.awrite('charge qty')
            xlh.awrite(proj.charge_qty, nextrow=True)
            xlh.awrite('charge ratetype')
            xlh.awrite(proj.charge_ratetype, nextrow=True)
            xlh.awrite('charge cycle')
            xlh.awrite(proj.charge_cycle, nextrow=True)
            xlh.awrite('charge start date')
            xlh.awrite(proj.charge_start, nextrow=True)
            xlh.awrite(nextrow=True)

---
+++ tests/test_numbers.py
@@ -0,0 +1,6 @@
+from blazeutils import moneyfmt
+from blazeutils.numbers import decimalfmt
+
+def test_moneyfmt():
+    assert moneyfmt('-0.02', neg='<', trailneg='>') == '<0.02>'
+    assert decimalfmt('-1234567.8901', places=0, sep='.', dp='', neg='', trailneg='-') == '1.234.568-'
