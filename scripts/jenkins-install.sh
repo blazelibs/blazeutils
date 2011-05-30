c5d74ebcbc01ad418a2a19cd0ad0a0c4b38d5949
Randy Syring <rsyring@gmail.com>
2011-05-30 10:11:58 -0400

    fix Jenkins build script and add Jenkins install script

---
+++ scripts/jenkins-install.sh
@@ -0,0 +1,16 @@
+PKGNAME="BlazeUtils"
+VENVDIR="$WORKSPACE/.venv-install"
+
+cd $WORKSPACE
+
+# remove the install venv so we start from scratch
+rm -rf "$VENVDIR"
+
+# create a new virtualenv
+virtualenv "$VENVDIR" --no-site-packages -q
+
+# activate virtualenv
+source "$VENVDIR/bin/activate"
+
+# install from pypi
+pip install "$PKGNAME"
