39eb213ae28d219b4702272017f7dfbafb211600
Randy Syring <rsyring@gmail.com>
2011-06-09 23:11:28 -0400

    add ensure_date()

--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -1,6 +1,6 @@
 import datetime as dt
 
-from blazeutils.dates import safe_strftime, ensure_datetime
+from blazeutils.dates import safe_strftime, ensure_datetime, ensure_date
 
 def test_safe_strftime():
     assert safe_strftime(None) == ''
@@ -14,3 +14,10 @@ def test_ensure_datetime():
     assert isinstance(nd, dt.datetime)
     assert dt.time(0, 0, 0) == nd.time()
     assert d == nd.date()
+
+def test_ensure_date():
+    dtm = dt.datetime.now()
+    d = dt.date.today()
+
+    assert ensure_date(dtm) == dtm.date()
+    assert ensure_date(d) is d
