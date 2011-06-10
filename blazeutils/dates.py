deb79eec1ef8789996161ac5169559acf5452704
Randy Syring <rsyring@gmail.com>
2011-06-09 23:00:22 -0400

    Deprecated datetime module, move to dates module, add ensure_datetime()

---
+++ blazeutils/dates.py
@@ -0,0 +1,18 @@
+# absolute import is necessary or datetime import will give
+# blazeutils.datetime instead
+from __future__ import absolute_import
+import datetime as dt
+
+def safe_strftime(value, format='%m/%d/%Y %H:%M', on_none=''):
+    if value is None:
+        return on_none
+    return value.strftime(format)
+
+def ensure_datetime(dobj):
+    """
+        Adds time part to dobj if its a date object, returns dobj
+        untouched if its a datetime object.
+    """
+    if isinstance(dobj, dt.datetime):
+        return dobj
+    return dt.datetime.combine(dobj, dt.time())
