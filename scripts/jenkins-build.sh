8cc243053bc9ac2cbe1d25a25f8292ccdaa36073
Randy Syring <rsyring@gmail.com>
2011-06-11 19:47:09 -0400

    updating jenkins related files

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -1,21 +1,27 @@
 PKGNAME="BlazeUtils"
-VENVDIR="$WORKSPACE/.venv-dev"
-
-cd $WORKSPACE
-
-# setup a virtualenv
-if [ -d "$VENVDIR" ]; then
-    echo "**> virtualenv exists"
+# script should be run from package root if not being run in Jenkins
+if [ -z "$WORKSPACE" ]; then
+    VENVDIR="/tmp/$PACKAGE-jenkins-venv"
 else
-    echo "**> creating virtualenv"
-    virtualenv "$VENVDIR" --no-site-packages -q
+    VENVDIR="$WORKSPACE/.venv-dev"
+    cd "$WORKSPACE"
 fi
 
-# activate virtualenv
-source "$VENVDIR/bin/activate"
+# delete the virtualenv, this ensures all packages are installed from scratch
+# to make sure our dependencies are specified correctly
+rm -rf "$VENVDIR"
+
+# create the venv
+virtualenv "$VENVDIR" --no-site-packages -q
+
+# If in jenkins, assuming the PATH has been set
+# correctly already, otherwise activate the VENV
+if [ -z "$WORKSPACE" ]; then
+    source "$VENVDIR/bin/activate"
+fi
 
 # install test requirements
-pip install -r requirements-testing.txt
+pip install -r pip-jenkins-reqs.txt
 
 # install as dev package
 python setup.py --quiet develop
