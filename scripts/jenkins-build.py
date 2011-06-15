3907d96528e0fceda1c354b995f844abbb9ecf63
Randy Syring <rsyring@gmail.com>
2011-06-15 14:36:07 -0400

    new jenkins build script

--- scripts/jenkins-build.py
+++ scripts/jenkins-build.py
@@ -9,19 +9,20 @@ bh = BuildHelper(package, type)
 # delete & re-create the venv
 bh.venv_create()
 
-# install test requirements, use EI for coverage so we get pre-compiled version
-# on windows
+# use easy_install for coverage so we get pre-compiled version on windows
 #
-# version (have to do version 3.4 b/c nosexcover will fail with newer version)
+# Also, have to do version 3.4 b/c nosexcover will fail with newer version)
 # bug report for that: https://github.com/cmheisel/nose-xcover/issues/5
-bh.vecall('easy_install', 'coverage==3.4')
+bh.vepycall('easy_install', 'coverage==3.4')
+
+# install other jenkins requirements
 bh.pip_install_reqs('pip-jenkins-reqs.txt')
 
 # install package w/ setuptools develop
 bh.setuppy_develop()
 
 # run tests & coverage
-bh.vecall(
+bh.vepycall(
     'nosetests', 'tests', '--with-xunit',
     '--with-coverage', '--cover-package=blazeutils', '--cover-tests',
     '--with-xcoverage',
