5474e393d9f0dc7820f458f780d601adc44d68f0
Randy Syring <rsyring@gmail.com>
2011-06-15 02:57:22 -0400

    install coverage w/ easy_install
    that way we get pre-compiled binaries on Windows

--- scripts/jenkins-build.py
+++ scripts/jenkins-build.py
@@ -9,7 +9,12 @@ bh = BuildHelper(package, type)
 # delete & re-create the venv
 bh.venv_create()
 
-# install test requirements
+# install test requirements, use EI for coverage so we get pre-compiled version
+# on windows
+#
+# version (have to do version 3.4 b/c nosexcover will fail with newer version)
+# bug report for that: https://github.com/cmheisel/nose-xcover/issues/5
+bh.vecall('easy_install', 'coverage==3.4')
 bh.pip_install_reqs('pip-jenkins-reqs.txt')
 
 # install package w/ setuptools develop
