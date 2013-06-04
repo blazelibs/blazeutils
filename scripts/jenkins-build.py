0c3413ed01953eebe611a6aeb34a05e3c49988e4
Randy Syring <rsyring@gmail.com>
2013-06-04 16:24:37 -0400

    specific mock version was causing automated testing problems

--- scripts/jenkins-build.py
+++ scripts/jenkins-build.py
@@ -12,10 +12,6 @@ bh.venv_create()
 # use easy_install for coverage so we get pre-compiled version on windows
 bh.vepycall('easy_install', 'coverage')
 
-# have to install dev build of mock using pip directly because of pip bug:
-# https://github.com/pypa/pip/issues/145
-bh.vepycall('pip', 'install', 'http://www.voidspace.org.uk/downloads/mock-0.8.0rc2.tar.gz#egg=mock-dev')
-
 # install other jenkins requirements
 bh.pip_install_reqs('pip-jenkins-reqs.txt')
 
