9c05baa77bffb919251ab6a3828da0bfdc73a7cd
Randy Syring <randy.syring@level12.io>
2015-03-18 16:03:43 -0400

    fix random test failures with ordered dicts
    caused by using kwargs to pass in initial values...Python sends those
    in as an unordered list, so sometimes the objects were not getting
    initialized in the order expected.

--- tests/test_datastructures.py
+++ tests/test_datastructures.py
@@ -1,15 +1,18 @@
 import cPickle as pickle
-
+import random
 
 from blazeutils.datastructures import DumbObject, OrderedProperties, OrderedDict, \
-    HtmlAttributeHolder, LazyOrderedDict, LazyDict, UniqueList
+    LazyOrderedDict, LazyDict, UniqueList
+
 
 # leave this here it ensures that LazyDict is available from .datastructures
 # even though the code was moved to .containers.
 assert LazyDict
 
+
 def test_dumbobject():
     assert DumbObject(a=1).a == 1
+
 
 def test_orderedproperties():
     o = OrderedProperties()
@@ -21,49 +24,69 @@ def test_orderedproperties():
         else:
             assert attr == 2
 
-def test_ordereddict():
-    o = OrderedDict(a=1, b=2)
-    for i, v in enumerate(o.values()):
-        if i == 0:
-            assert v == 1
-        else:
-            assert v == 2
 
-def test_ordereddict_pickle():
-    o = OrderedDict(a=1, b=2)
+class TestOrderedDict(object):
 
-    ostr = pickle.dumps(o, pickle.HIGHEST_PROTOCOL)
-    o = pickle.loads(ostr)
+    def test_ordereddict(self):
+        o = OrderedDict((('a', 'a'), ('b', 'b')))
 
-    for i, v in enumerate(o.values()):
-        if i == 0:
-            assert v == 1
-        else:
-            assert v == 2
+        # add a bunch of random junk so that a passing test is less likely to be accidentally valid
+        for value in range(random.randint(100, 1000)):
+            o[value] = value
 
+        o['c'] = 'c'
 
-def test_ordereddict_lazy():
+        for i, v in enumerate(o.values()):
+            if i == 0:
+                assert v == 'a'
+            elif i == 1:
+                assert v == 'b'
 
-    o = LazyOrderedDict(a=1, b=2)
-    for i, v in enumerate(o.values()):
-        if i == 0:
-            assert v == 1
-        else:
-            assert v == 2, v
-    assert o.a == 1
-    assert o.b == 2
-    o.c = 3
-    assert o.c == 3, o.c
-    assert o['c'] == 3
-    try:
-        o.d
-        assert False
-    except AttributeError:
-        pass
+        assert o.values()[-1] == 'c'
+
+    def test_ordereddict_pickle(self):
+        o = OrderedDict((('a', 'a'), ('b', 'b')))
+
+        # add a bunch of random junk so that a passing test is less likely to be accidentally valid
+        for value in range(random.randint(100, 1000)):
+            o[value] = value
+
+        o['c'] = 'c'
+
+        ostr = pickle.dumps(o, pickle.HIGHEST_PROTOCOL)
+        o = pickle.loads(ostr)
+
+        for i, v in enumerate(o.values()):
+            if i == 0:
+                assert v == 'a'
+            elif i == 1:
+                assert v == 'b'
+
+        assert o.values()[-1] == 'c'
+
+    def test_ordereddict_lazy(self):
+
+        o = LazyOrderedDict((('a', 'a'), ('b', 'b')))
+        for i, v in enumerate(o.values()):
+            if i == 0:
+                assert v == 'a'
+            elif i == 1:
+                assert v == 'b', v
+        assert o.a == 'a'
+        assert o.b == 'b'
+        o.c = 3
+        assert o.c == 3, o.c
+        assert o['c'] == 3
+        try:
+            o.d
+            assert False
+        except AttributeError:
+            pass
+
 
 def test_unique_list():
     ul = UniqueList([1, 1, 2, 2, 3])
     ul.append(2)
     ul.append(5)
-    ul.extend([6,7, 7, 1])
-    assert ul == [1,2,3,5,6,7]
+    ul.extend([6, 7, 7, 1])
+    assert ul == [1, 2, 3, 5, 6, 7]
