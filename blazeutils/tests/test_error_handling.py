878c711d5dc964552b68364d97ccd52e36c500b5
Randy Syring <randy.syring@level12.io>
2015-03-20 19:51:45 -0400

    error handling py3 conversion

--- blazeutils/tests/test_error_handling.py
+++ blazeutils/tests/test_error_handling.py
@@ -1,32 +1,35 @@
-from blazeutils import tb_depth_in, traceback_depth
-from blazeutils.error_handling import _uie_matches
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from blazeutils.error_handling import tb_depth_in, traceback_depth, _uie_matches
 from blazeutils.testing import emits_deprecation
 
 
 @emits_deprecation('.+its a bad idea')
 def test_traceback_funcs():
     try:
-        import somethingthatwontbethereihope
+        import somethingthatwontbethereihope  # noqa
         assert False, 'expected import error'
     except ImportError:
-        pass
-    assert traceback_depth() == 0, 'if this test fails, you probably have something wrapping __import__'
+        assert traceback_depth() == 0, 'if this test fails, you probably have something wrapping' \
+            '__import__'
 
     try:
-        from _bad_import import foobar
+        from ._bad_import import foobar  # noqa
+        assert False, 'expected Import Error'
     except ImportError:
-        pass
-    assert traceback_depth() == 0
+        assert traceback_depth() == 0
 
     try:
-        from _bad_import_deeper import foobar
+        from ._bad_import_deeper import foobar2  # noqa
+        assert False, 'expected import error'
     except ImportError:
-        pass
-    assert traceback_depth() == 1
+        assert traceback_depth() == 1
 
-    assert tb_depth_in(1)
-    assert tb_depth_in((0,1))
-    assert not tb_depth_in((3,4))
+        assert tb_depth_in(1)
+        assert tb_depth_in((0, 1))
+        assert not tb_depth_in((3, 4))
+
 
 class TestRaiseUIE(object):
 
@@ -34,7 +37,9 @@ class TestRaiseUIE(object):
         assert _uie_matches('minimal1.events', 'No module named events')
 
     def test_two(self):
-        assert _uie_matches('minimal2.components.internalonly.events', 'No module named internalonly.events')
+        assert _uie_matches('minimal2.components.internalonly.events',
+                            'No module named internalonly.events')
 
     def test_end_matches_but_not_beginning(self):
-        assert not _uie_matches('tcsdata.components.rem.model.orm', 'No module named tcdata.components.tcssite.model.orm')
+        assert not _uie_matches('tcsdata.components.rem.model.orm',
+                                'No module named tcdata.components.tcssite.model.orm')
