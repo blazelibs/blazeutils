66d0ca8cd688509981df360e2c945b092d440131
Randy Syring <randy.syring@level12.io>
2015-03-20 16:06:33 -0400

    increasing datastructures test coverage

--- blazeutils/tests/test_datastructures.py
+++ blazeutils/tests/test_datastructures.py
@@ -8,8 +8,7 @@ import six.moves.cPickle as pickle
 
 from blazeutils.datastructures import DumbObject, OrderedProperties, OrderedDict, \
     LazyOrderedDict, LazyDict, UniqueList
-
-
+from blazeutils.testing import raises
 
 # leave this here it ensures that LazyDict is available from .datastructures
 # even though the code was moved to .containers.
@@ -20,15 +19,112 @@ def test_dumbobject():
     assert DumbObject(a=1).a == 1
 
 
-def test_orderedproperties():
-    o = OrderedProperties()
-    o.a = 1
-    o.b = 2
-    for i, attr in enumerate(o):
-        if i == 0:
-            assert attr == 1
-        else:
-            assert attr == 2
+class TestOrderedProps(object):
+    def test_basics(self):
+        o = OrderedProperties()
+        o.a = 1
+        o.b = 2
+        for i, attr in enumerate(o):
+            if i == 0:
+                assert attr == 1
+            else:
+                assert attr == 2
+
+
+    def test_list(self):
+        o = OrderedProperties()
+        o.a = 1
+        o.b = 2
+
+        assert list(o) == [1, 2]
+
+    def test_add(self):
+        o1 = OrderedProperties()
+        o2 = OrderedProperties()
+
+        o1.a = 1
+        o1.c = 3
+        o2.b = 2
+        o2.d = 4
+
+        result = o1 + o2
+
+        assert result == [1, 3, 2, 4]
+
+    def test_del(self):
+        o = OrderedProperties()
+        assert 'a' not in o
+        o.a = 1
+        assert 'a' in o
+        del o.a
+        assert 'a' not in o
+
+    def test_del_item(self):
+        o = OrderedProperties()
+        assert 'a' not in o
+        o.a = 1
+        assert 'a' in o
+        del o['a']
+        assert 'a' not in o
+
+    def test_pickle(self):
+        o = OrderedProperties()
+        o.a = 1
+        o.b = 2
+
+        ostr = pickle.dumps(o, pickle.HIGHEST_PROTOCOL)
+        po = pickle.loads(ostr)
+
+        assert po.a == 1
+        assert po.b == 2
+        assert list(po) == [1, 2]
+
+    @raises(AttributeError, 'foo')
+    def test_attribute_error(self):
+        o = OrderedProperties()
+        o.foo
+
+    @raises(AttributeError, 'foo')
+    def test_del_attribute_error(self):
+        o = OrderedProperties()
+        del o.foo
+
+    def test_initilization(self):
+        o = OrderedProperties(initialize=False)
+        o.a = 1
+        o._initialized = True
+        o.b = 2
+
+        assert o.a == 1
+        assert list(o) == [2]
+
+        del o.a
+
+        try:
+            o.a
+            assert False, 'expected exception'
+        except AttributeError:
+            pass
+
+    def test_update(self):
+        o = OrderedProperties()
+        o.a = 1
+        o.update(dict(b=2))
+        assert o.b == 2
+
+        assert list(o) == [1, 2]
+
+    def test_has_key(self):
+        o = OrderedProperties()
+        o.a = 1
+        assert o.has_key('a')
+
+    def test_clear(self):
+        o = OrderedProperties()
+        o.a = 1
+        assert 'a' in o
+        o.clear()
+        assert 'a' not in o
 
 
 class TestOrderedDict(object):
