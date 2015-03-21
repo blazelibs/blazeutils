2c75cdd50b45b50ad1ce5f2a2994e4a5d619f996
Randy Syring <randy.syring@level12.io>
2015-03-20 23:26:52 -0400

    convert numbers testing

--- blazeutils/tests/test_numbers.py
+++ blazeutils/tests/test_numbers.py
@@ -1,6 +1,8 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
 
-from blazeutils import moneyfmt
-from blazeutils.numbers import decimalfmt, ensure_int, convert_int
+from blazeutils.numbers import moneyfmt, decimalfmt, ensure_int, convert_int
+
 
 def test_moneyfmt():
     assert moneyfmt('-0.02', neg='<', trailneg='>') == '<0.02>'
@@ -8,14 +10,16 @@ def test_moneyfmt():
     # handles floats too
     assert decimalfmt(-1234567.8901) == '-1,234,567.89'
 
+
 def test_ensure_int():
     assert ensure_int(10) == 10
     assert ensure_int('10') == 10
     assert ensure_int('foo') == 0
     assert ensure_int(None) == 0
 
+
 def test_convert_int():
     assert convert_int(10) == 10
     assert convert_int('10') == 10
-    assert convert_int('foo') == None
-    assert convert_int(None) == None
+    assert convert_int('foo') is None
+    assert convert_int(None) is None
