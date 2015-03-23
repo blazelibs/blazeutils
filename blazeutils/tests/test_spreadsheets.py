007707e9d02795367bb6b9d21e47b778e75d95dd
Randy Syring <randy.syring@level12.io>
2015-03-23 16:26:12 -0400

    spreadsheets: python3 conversion and styling

--- blazeutils/tests/test_spreadsheets.py
+++ blazeutils/tests/test_spreadsheets.py
@@ -1,4 +1,8 @@
-import xlwt
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import pytest
+import six
 
 from blazeutils.spreadsheets import workbook_to_reader, XlwtHelper, http_headers
 from blazeutils.testing import emits_deprecation
@@ -6,8 +10,9 @@ from blazeutils.testing import emits_deprecation
 
 class TestWorkbookToReader(object):
 
-    def test_it(self):
-
+    @pytest.mark.skipif(not six.PY2, reason="xlwt only works on Python 2")
+    def test_xlwt_to_reader(self):
+        import xlwt
         write_wb = xlwt.Workbook()
         ws = write_wb.add_sheet('Foo')
         ws.write(0, 0, 'bar')
@@ -20,6 +25,7 @@ class TestWorkbookToReader(object):
 class TestWriter(object):
 
     @emits_deprecation('XlwtHelper has been renamed to Writer')
+    @pytest.mark.skipif(not six.PY2, reason="xlwt only works on Python 2")
     def test_xlwt_helper_deprecation(self):
         XlwtHelper()
 
