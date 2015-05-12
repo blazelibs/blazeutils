4196b2da2553d43d903017fa1ad60a9178868397
mlewellyn <matt.lewellyn@level12.io>
2015-05-12 09:37:04 -0400

    fixed case where error handling masked real exception by creating one

--- blazeutils/error_handling.py
+++ blazeutils/error_handling.py
@@ -48,5 +48,8 @@ _dotted_path_rx = re.compile(r'{0}(\.{0})*$'.format(_identifier), re.UNICODE)
 
 def _uie_matches(our_import, exc_str):
     match = _dotted_path_rx.search(exc_str)
+    # make sure there is a match before using .group attr
+    if match is None:
+        return False
     dotted_part = match.group(0)
     return our_import.endswith(dotted_part)
